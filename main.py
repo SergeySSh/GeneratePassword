f4666d24a83dd642e66dd6c925ebf7010d4e082e
SergeySSh <sssvsssv1@mail.ru>
2024-12-07 00:11:12 +0400

    second variable

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 """Программа генерирует пароли любой сложности и длины, соответствующей заданным условиям."""
-from random import randint, choice
+from random import choice
 
-numbers = [48, 58]  # цифры
-simbols = [33, 48]  # символы
-letters = [65, 90]  # буквы верхнего регистра
-letters_lower = [97, 122]  # буквы нижнего регистра
-letters_rus = [1040, 1072]  # русские буквы
-letters_rus_lower = [1072, 1104]  # русские буквы нижнего регистра
+numbers = [i for i in range(48, 65)]  # цифры
+simbols = [i for i in range(33, 49)]  # символы
+letters = [i for i in range(65, 91)]  # буквы верхнего регистра
+letters_lower = [i for i in range(97, 123)]  # буквы нижнего регистра
+letters_rus = [i for i in range(1040, 1073)]  # русские буквы верхнего регистра
+letters_rus_lower = [i for i in range(1072, 1105)]  # русские буквы нижнего регистра
 
 
 def generate_password(length: int, complexity: str):
     """Генерирует пароль."""
-    if complexity == 'easy':
+    if complexity == 'simple':
+        complexity_symbols = numbers + simbols
+    elif complexity == 'easy':
         complexity_symbols = numbers + simbols + letters_lower
     elif complexity == 'normal':
         complexity_symbols = numbers + simbols + letters + letters_lower
     elif complexity == 'hard':
         complexity_symbols = numbers + simbols + letters + letters_lower + letters_rus + letters_rus_lower
     else:
-        raise ValueError('Invalid complexity level. Choose from easy, normal, hard.')
+        raise ValueError('Invalid complexity level. Choose from simple, easy, normal, hard.')
 
     password = []
     for _ in range(length):
@@ -29,7 +31,7 @@ def generate_password(length: int, complexity: str):
 
 def main():
     length = int(input('Enter password length: '))
-    complexity = input('Enter complexity level (easy, normal, hard): ').lower()
+    complexity = input('Enter complexity level (simple, easy, normal, hard): ').lower()
     print('Generated password:', generate_password(length, complexity))
 
 
